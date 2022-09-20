ab564ae8fc9c190fd978069933adf8d5b667b635
valteruo <valter@LAPTOP-P5UO1VRQ.localdomain>
2022-09-20 19:01:56 +0300

    Correct loss and accurary for multi-class classification

--- utils.py
+++ utils.py
@@ -17,6 +17,12 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
         
+        
+def normalise(predictions):
+    # apply smoothing to predictions
+    predictions = np.abs(predictions) + 1e-9
+    return predictions / predictions.sum()
+        
 
 def create_labeled_classes(data, classification):
     labeled_data = {}
@@ -30,13 +36,30 @@ def create_labeled_classes(data, classification):
     return labeled_data
 
 
-def acc(y_hat, y):
-    y_hat = np.array(y_hat).flatten()
-    y = np.array(y).flatten()
+def bin_class_acc(y_hat, y):
+    y_hat = np.array(y_hat)
+    y = np.array(y)
     return (np.sum(np.round(y_hat) == y) / len(y)) / 2
 
 
-def loss(y_hat, y):
-    y_hat = np.array(y_hat).flatten()
-    y = np.array(y).flatten()
+def bin_class_loss(y_hat, y):
+    y_hat = np.array(y_hat)
+    y = np.array(y)
     return -np.sum(y * np.log(y_hat)) / len(y)
+
+
+def multi_class_acc(y_hat, y):
+    total_acc = 0
+    for pair in zip(y_hat, y):
+        y_meas = np.array(pair[0]).flatten()
+        max_index = np.argmax(y_meas)
+        total_acc += int(int(pair[1][max_index]) == 1)
+    return total_acc / len(y)
+
+
+def multi_class_loss(y_hat, y):
+    total_loss = 0
+    for pair in zip(y_hat, y):
+        y_meas = np.array(pair[0]).flatten()
+        total_loss += -np.sum(pair[1] * np.log(y_meas)) / len(pair[1])
+    return total_loss
